eda34ea9a2cbc61f63f920559ae47ec20794782a
sarathasoka <55868074+sarath-asoka@users.noreply.github.com>
2023-04-25 16:47:42 +0900

    Update inverted pendulum.cpp

--- Day 1/inverted pendulum.cpp
+++ Day 1/inverted pendulum.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void c2d(double A[4][4], double Anew[4][4]);
 void x_new(double x_init[4][1], double Anew[4][4], double x_next[4][1]);
+void update_init(double x_init[4][1], double x_next[4][1]);
 
 int main(){
 	double A[4][4] = {
@@ -16,17 +17,20 @@ int main(){
 	double Anew[4][4];
     c2d(A,Anew);
     double x_init[4][1] = {{0},{0},{3.14/2},{0}};
-    double x_next[4][1] ={{0},{0},{0},{0}};
-    x_new(x_init,Anew,x_next);    
+    
 
-    cout << "Next state"<<endl;
-    for (int i=0;i<4;i++){
-        for (int j=0;j<1;j++){
-            cout << x_next[i][j] <<" ";
-        }
-        cout << endl;
+    for (int t = 0; t<5; t++){
+        double x_next[4][1] ={{0},{0},{0},{0}};
+
+        x_new(x_init,Anew,x_next);    
+        update_init(x_init,x_next);
+
+        cout << endl<< "Next state : "<<t<<endl;
+        for (int i=0;i<4;i++){
+            for (int j=0;j<1;j++){
+                cout << x_next[i][j] <<" ";
+        }}        
     }
-
     return 0;
 }
 
@@ -59,4 +63,12 @@ void x_new(double x_init[4][1], double Anew[4][4],double x_next[4][1]){
             }
         }
     }    
+}
+
+void update_init(double x_init[4][1], double x_next[4][1]){
+    for (int i=0; i<4; i++){
+        for (int j=0; j<1; j++){
+            x_init[i][j] = x_next[i][j];
+        }
+    }
 }
