ff16130cf5adf1e2e4a707822812a82235ac07a3
sarathasoka <55868074+sarath-asoka@users.noreply.github.com>
2023-04-25 15:51:00 +0900

    Update inverted pendulum.cpp

--- Day 1/inverted pendulum.cpp
+++ Day 1/inverted pendulum.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<cmath>
-using namespace std; 
-main( ) 
-{  
+using namespace std;
+
+void c2d(double A[4][4], double Anew[4][4]);
+void x_new(double x_init[4][1], double Anew[4][4], double x_next[4][1]);
+
+int main(){
 	double A[4][4] = {
 		{ 0, 1, 0, 0},
 		{ 0, -0.9091, 2.6755, 0},
@@ -10,10 +13,24 @@ main( )
 		{ 0, -2.2727, 31.2136, 0}
 	} ;
 
-	double B[4][1] = {
-        {0},{1.8182},{0},{4.5455}
-    };
+	double Anew[4][4];
+    c2d(A,Anew);
+    double x_init[4][1] = {{0},{0},{3.14/2},{0}};
+    double x_next[4][1] ={{0},{0},{0},{0}};
+    x_new(x_init,Anew,x_next);    
 
+    cout << "Next state"<<endl;
+    for (int i=0;i<4;i++){
+        for (int j=0;j<1;j++){
+            cout << x_next[i][j] <<" ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+void c2d(double A[4][4], double Anew[4][4]){
     int I[4][4];
     for(int i=0; i<4; i++){
         for(int j=0; j<4; j++){
@@ -25,58 +42,21 @@ main( )
             }
         }
     }
-	
     float dt = 0.1;
-
-    double Anew[4][4];
-
     for (int i=0; i<4; i++){
         for (int j=0; j<4; j++){
             Anew[i][j] = I[i][j] + dt*A[i][j];
         }
-    }
+    }    
+}
 
-    double x_init[4][1] = {{0},{0},{3.14/2},{0}};
-    double x_next[4][1];
-    int tend = 1;
-    float time = tend/dt;
-
-
+void x_new(double x_init[4][1], double Anew[4][4],double x_next[4][1]){
     for(int i= 0; i<4; i++){
         for(int j=0; j<1; j++){
             for (int k=0; k < 4; ++k)
             {
-                x_next[i][j] += A[i][k]*x_init[k][j];
+                x_next[i][j] += Anew[i][k]*x_init[k][j];
             }
         }
-    }
-
-
-
-/***
-    for(int t=0;t<time; t++){
-        for(int i= 0; i<4; i++){
-            for(int j=0; j<4; j++){
-                for (int k=0; k < 2; ++k)
-                {
-                    x_next[i][j] += Anew[i][k]*x_init[k][j];
-                }
-                x_init[i][j] = x_next[i][j];
-            }
-        }
-    }
-***/
-//    /***
-	cout<<"Printing a 2D Array:\n";
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<1;j++)
-		{
-			cout<<"\t"<<x_next[i][j];
-		}
-		cout<<endl;
-	}
-//    ***/
-    
-
+    }    
 }
