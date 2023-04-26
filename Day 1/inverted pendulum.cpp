3fde66d842ec0c0553ff56ab7ce36e58fd551d1c
sarathasoka <55868074+sarath-asoka@users.noreply.github.com>
2023-04-26 12:54:57 +0900

    Update inverted pendulum.cpp

--- Day 1/inverted pendulum.cpp
+++ Day 1/inverted pendulum.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 void c2d(double A[4][4], double Anew[4][4]);
-void x_new(double x_init[4][1], double Anew[4][4], double x_next[4][1]);
+void ctod(double B[4][1], double Bnew[4][1]);
+void x_new(double x_init[4][1], double Anew[4][4], double x_next[4][1], double Bnew[4][1], double K[1][4]);
 void update_init(double x_init[4][1], double x_next[4][1]);
 
 int main(){
@@ -13,23 +14,29 @@ int main(){
 		{ 0, 0, 0, 1},
 		{ 0, -2.2727, 31.2136, 0}
 	} ;
-
+	double B[4][1] = {
+        {0},{1.8182},{0},{4.5455}
+    };
 	double Anew[4][4];
     c2d(A,Anew);
-    double x_init[4][1] = {{0},{0},{3.14/2},{0}};
+    double Bnew[4][1];
+    ctod(B,Bnew);
     
-
-    for (int t = 0; t<5; t++){
+    double x_init[4][1] = {{0},{0},{3.14/2},{0}};
+    double K[1][4] = {-1, -1.6567, 18.6854, 3.4594};
+    cout<< x_init[2][0];
+    for (int t = 0; t<157; t++){
         double x_next[4][1] ={{0},{0},{0},{0}};
-
-        x_new(x_init,Anew,x_next);    
+        x_new(x_init,Anew,x_next,Bnew,K);    
         update_init(x_init,x_next);
-
-        cout << endl<< "Next state : "<<t<<endl;
-        for (int i=0;i<4;i++){
+        // cout << "Error :" << x_next[2][0] <<endl;
+        // cout << endl<< "Next state : "<<t<<endl;
+/*         for (int i=0;i<4;i++){
             for (int j=0;j<1;j++){
                 cout << x_next[i][j] <<" ";
-        }}        
+        }
+        }
+        cout << endl; */
     }
     return 0;
 }
@@ -54,12 +61,12 @@ void c2d(double A[4][4], double Anew[4][4]){
     }    
 }
 
-void x_new(double x_init[4][1], double Anew[4][4],double x_next[4][1]){
+void x_new(double x_init[4][1], double Anew[4][4], double x_next[4][1], double Bnew[4][1], double K[1][4]){
     for(int i= 0; i<4; i++){
         for(int j=0; j<1; j++){
             for (int k=0; k < 4; ++k)
             {
-                x_next[i][j] += Anew[i][k]*x_init[k][j];
+                x_next[i][j] += (Anew[i][k]-Bnew[i][j]*K[j][k])*x_init[k][j];
             }
         }
     }    
@@ -71,4 +78,13 @@ void update_init(double x_init[4][1], double x_next[4][1]){
             x_init[i][j] = x_next[i][j];
         }
     }
+}
+
+void ctod(double B[4][1], double Bnew[4][1]){
+    float dt = 0.1;
+    for (int i=0; i<4; i++){
+        for (int j=0; j<1; j++){
+            Bnew[i][j] = dt*B[i][j];
+        }
+    }       
 }
